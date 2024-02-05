c4abdcdd884a902965bf199f597a9c7f0369d7b4
Muhammad Abdur Raafay <115172845+Raafay009@users.noreply.github.com>
2024-02-05 21:48:53 +0500

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/bridge.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Image of a bridge";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a Software Engineering Student at UET Taxila with a passion for Web Development and professinal experience in Graphic Designing.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -34,8 +34,8 @@ const skillsList = [
   "Web design",
   "User experience",
   "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Web Development ",
+  "Customer Support",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my graphic design and web development experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/snow.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "picture of snow";
 
 /**
  * Project list
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Portfolio Website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is a basic portfolio app built by me using HTML and CSS.",
+    url: "https://wajeeha02.github.io/portfoliowajeeha/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Art By Wajeeha",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is an Instagram account where I post my creative work I build as a side passion.",
+    url: "https://www.instagram.com/artbywajeeha/",
   },
   {
-    title: "My Resume Site",
+    title: "Secure Scope",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A project developed by me and my team as part of Google Developers Solution Challenge (2023).",
+    url: "https://scope-3f3e3.web.app/",
   },
 ];
 
